e7069d04c5ab428a373db0112fcd5e3e712ac807
anmol963 <98901389+anmol963@users.noreply.github.com>
2022-02-10 21:47:09 +0530

    Add files via upload

---
+++ hungry.py
@@ -0,0 +1,13 @@
+hungry=input("are you hungry")
+if hungry=="yes":
+    print("Eat samosa")
+    print("eat pizza")
+    print("eat burger")
+    print("eat fries")
+    print("eat pie")
+else:
+    thirsty=input("are you thirsty")
+    if thirsty=="yes":
+        print("drink water")
+        print("drink soda")
+    
